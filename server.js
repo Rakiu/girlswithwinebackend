c733c960dc52e32b423d9d8f0bc867294cb99572
Rakiu <sainiravindra369@gmail.com>
2026-05-16 20:16:13 +0530

    add

--- server.js
+++ server.js
@@ -32,7 +32,17 @@ const __dirname = path.dirname(__filename);
 /* ==============================
    MIDDLEWARE
 ============================== */
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://girlswithwine.com",
+      "https://www.girlswithwine.com",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
